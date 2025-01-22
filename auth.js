26698643fc5919667844120e52cfcb293d81338d
Rafasaeza <rafasaezarana@gmail.com>
2025-01-22 22:19:29 +0100

    Quito proveederes de google(Creo que lo hace automático)

--- auth.js
+++ auth.js
@@ -1,10 +1,7 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  })],
+  providers: [Google],
   callbacks: {
     async signIn({user,account}){
         console.log("User:",user);
