6c536246f31cc50d652cdd47d42d070b0d6fb183
Rafasaeza <rafasaezarana@gmail.com>
2025-01-22 22:46:55 +0100

    manejo de imagenes cambiado para producción

--- app/api/image-upload/route.js
+++ app/api/image-upload/route.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { NextResponse } from 'next/server';
-import path from 'path';
-import { writeFile } from 'fs/promises';
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -11,25 +10,33 @@ cloudinary.config({
 export async function POST(request) {
   try {
     const formData = await request.formData();
-   
     const file = formData.get('file');
 
     if (!file) {
       return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
     }
 
-    //almaceno los bytes en memoria
-    const bytes = await file.arrayBuffer()
-    const buffer = Buffer.from(bytes)
-    //Guardo el archivo en el servidor
-    const filePath = path.join(process.cwd(), 'public', file.name)
-    await writeFile(filePath, buffer)
+    // Almacenar el archivo en memoria
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-    const uploadResponse = await cloudinary.uploader.upload(filePath, {
-      folder: 'uploads',
+    // Usamos una promesa para asegurar que la respuesta se devuelve al finalizar la carga
+    const uploadResponse = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_stream(
+        { folder: 'uploads' },
+        (error, result) => {
+          if (error) {
+            console.error('Cloudinary upload error:', error);
+            reject('Failed to upload image');
+          } else {
+            resolve(result);
+          }
+        }
+      ).end(buffer);
     });
 
     return NextResponse.json({ url: uploadResponse.secure_url });
+
   } catch (error) {
     console.error('Upload error:', error);
     return NextResponse.json({ error: 'Failed to upload image' }, { status: 500 });
